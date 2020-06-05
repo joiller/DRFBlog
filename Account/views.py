6922d1eea76d1e7707df9f8b9627d5be66d7b1f3
jianghuilai <781225147@qq.com>
2020-06-05 17:07:18 +0800

    loginview  配合一下 template

--- Account/views.py
+++ Account/views.py
@@ -120,6 +120,15 @@ def register_result(request):
 class LoginView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        login(request, user)
+        return Response({'token': token.key})
+
 
 def logout_view(request):
     logout(request)
